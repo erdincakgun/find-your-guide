022a5894675484d1599a3c126214d78aea69bc6e
Erdinç Akgün <erdinc.akgun@icloud.com>
2024-07-06 18:24:00 +0300

    Add Docker ignore, update compose file and requirements

--- src/main.py
+++ src/main.py
@@ -1,12 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
 from flask import Flask, render_template
+from os import getenv
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DB_URI")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template("index.html")
 
 
 if __name__ == "__main__":
+    print(app.config["SQLALCHEMY_DATABASE_URI"])
     app.run("0.0.0.0", 80, debug=True)
