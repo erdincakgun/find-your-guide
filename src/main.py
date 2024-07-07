22f85754a727ae348b47737ff027bf8bd401d9b6
Erdinç Akgün <erdinc.akgun@icloud.com>
2024-07-07 13:17:04 +0300

    Add python-dotenv package to requirements.txt and load .env

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from flask import Flask, render_template
 from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = getenv("SECRET_KEY")
@@ -11,11 +15,21 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
+class Base(DeclarativeBase):
+    pass
+
+
+class Guide(Base):
+    __tablename__ = "guides"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[str] = mapped_column(String(45))
+    last_name: Mapped[str] = mapped_column(String(45))
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
 if __name__ == "__main__":
-    print(app.config["SQLALCHEMY_DATABASE_URI"])
     app.run("0.0.0.0", 80, debug=True)
